737019977d737d66af04801e2e63d808f34eb537
shiva shankar <shivashankar144@shivas-MacBook-Air.local>
2025-04-21 15:27:17 -0400

    Fix manifest + add missing auth routes

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import MergedRestaurantViewSet, dropdown_options
 
 merged_restaurant_view = MergedRestaurantViewSet.as_view({
@@ -10,6 +10,8 @@ restaurant_detail_view = MergedRestaurantViewSet.as_view({
 })
 
 urlpatterns = [
+    path('auth/', include('dj_rest_auth.urls')),  # ✅ Login, logout
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),  # ✅ Register
     path('merged_restaurants/', merged_restaurant_view, name='merged_restaurants'),
     path('merged_restaurants/<str:pk>/', restaurant_detail_view, name='restaurant_detail'),
     path('dropdown_options/', dropdown_options, name='dropdown_options'),
